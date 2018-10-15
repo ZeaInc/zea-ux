26e4b60b929d72c1e1e519d24a62c07e70aa5116
Philip Taylor <phil@hordesoftware.co>
2018-10-15 14:56:27 -0400

    Cleaned up change.

--- src/undoredo/Change.js
+++ src/undoredo/Change.js
@@ -1,15 +1,30 @@
 class Change {
 	constructor(name) {
-		this.name = name;
+		this.name = name ? name : this.constructor.name;
 	}
 
 	undo() {}
 
 	redo() {}
 
-	toJSON() {}
+  update(updateData) {}
 
-	fromJSON(j) {}
+	toJSON() { 
+		return {
+	   name: this.name
+   }
+  }
+
+	fromJSON(j) {
+    this.name = j.name;
+  }
+
+
+  changeFromJSON(j) {
+    // Many change objects can load json directly
+    // in the update method.
+    this.update(j);
+  }
 }
 
 export default Change;
