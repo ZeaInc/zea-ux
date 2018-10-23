feb011f7feb96d77863adc18df23dd648ede0bdc
Philip Taylor <phil@hordesoftware.co>
2018-10-23 15:47:00 -0400

    Base tool now inherits from ParameterOwner.

--- src/tools/BaseTool.js
+++ src/tools/BaseTool.js
@@ -1,31 +1,12 @@
 
-// Note: we can't import Visualive because
-// parcel will cause the package to be loaded 2x. 
-// The Visualive global is only created once 
-// the main index file is loaded.
-export default class BaseTool { // extends Visualive.ParameterOwner {
+export default class BaseTool extends Visualive.ParameterOwner {
   constructor(undoRedoManager) {
-    // super();
+    super();
     this.undoRedoManager = undoRedoManager;
 
     this.actionFinished = new Visualive.Signal();
 
     this.__params = []
-  }
-
-
-  addParameter(param) {
-      const name = param.getName();
-      // if(this.__paramMapping[name] != undefined) {
-      //     console.warn("Replacing Parameter:" + name)
-      //     this.removeParameter(name);
-      // }
-      // this.__paramSignalIds[name] = param.valueChanged.connect((mode) => this.parameterValueChanged.emit(param, mode));
-      // param.addRef(this);
-      this.__params.push(param)
-      // this.__paramMapping[name] = this.__params.length - 1;
-      // this.parameterAdded.emit(name);
-      return param;
   }
 
   /////////////////////////////////////
