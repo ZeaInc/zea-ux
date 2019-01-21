f55d458977b9db7c31558253a0d6fafe409a90f1
phil_visualive <phil@visualive.io>
2019-01-21 09:52:01 -0500

    added support for bottom panel.

--- src/ui/side-panels.js
+++ src/ui/side-panels.js
@@ -1,11 +1,8 @@
-class Panel {
+class SidePanel {
   constructor(domElement, handleElement, panelSide) {
     this.domElement = domElement;
 
-    handleElement.addEventListener('mousedown', initDrag, false);
-
     let startX, startWidth;
-
     function initDrag(e) {
       startX = e.clientX;
       startWidth = parseInt(
@@ -27,6 +24,8 @@ class Panel {
       document.removeEventListener('mousemove', doDrag, false);
       document.removeEventListener('mouseup', stopDrag, false);
     }
+
+    handleElement.addEventListener('mousedown', initDrag, false);
   }
 
   setPanelWidget(widget) {
@@ -39,13 +38,62 @@ class Panel {
   }
 }
 
+class BottomPanel {
+  constructor(domElement, handleElement) {
+    this.domElement = domElement;
+
+    let startY, startHeight;
+
+    function initDrag(e) {
+      startY = e.clientY;
+      startHeight = parseInt(
+        document.defaultView.getComputedStyle(domElement).height,
+        10
+      );
+      document.addEventListener('mousemove', doDrag, false);
+      document.addEventListener('mouseup', stopDrag, false);
+    }
+
+    function doDrag(e) {
+      const delta = e.clientY - startY;
+      let panelHeight = startHeight - delta;
+      if (panelHeight < 10) panelHeight = 0;
+      domElement.style.height = `${panelHeight}px`;
+    }
+
+    function stopDrag(e) {
+      document.removeEventListener('mousemove', doDrag, false);
+      document.removeEventListener('mouseup', stopDrag, false);
+    }
+
+    handleElement.addEventListener('mousedown', initDrag, false);
+  }
+
+  setPanelWidget(widget) {
+    if (this.widget) {
+      this.widget.unMount();
+    }
+
+    this.widget = widget;
+    this.widget.mount();
+  }
+}
+
+
 export default class Panels {
   constructor() {
     this.viewportElement = document.getElementById('viewport');
     const panelElements = document.getElementsByClassName('SidePanel');
     const panelHandlers = document.getElementsByClassName('PanelHandler');
 
-    this.leftPanel = new Panel(panelElements[0], panelHandlers[0], 0);
-    this.rightPanel = new Panel(panelElements[1], panelHandlers[1], 1);
+    this.leftPanel = new SidePanel(panelElements[0], panelHandlers[0], 0);
+    this.rightPanel = new SidePanel(panelElements[1], panelHandlers[1], 1);
+
+
+    const bottomPanelHandler = document.getElementsByClassName('BottomPanelHandler');
+    const bottomPanel = document.getElementById('bottomPanel');
+    if(bottomPanelHandler && bottomPanel) {
+      this.bottomPanel = new BottomPanel(bottomPanel, bottomPanelHandler[0]);
+    }
   }
 }
