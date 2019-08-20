180dce63a05a851904169c6965021454e950305f
mauroporras <mauroporrasc@gmail.com>
2019-08-20 08:20:23 -0500

    docs: Add examples

--- src/Action.js
+++ src/Action.js
@@ -1,4 +1,11 @@
+/** Class representing an action. */
 export default class Action {
+  /**
+   * Create an action.
+   * @param {any} name - The name value.
+   * @param {any} path - The path value.
+   * @param {boolean} availableInVR - The availableInVR value.
+   */
   constructor(name, path, availableInVR = false) {
     this.name = name;
     this.path = path;
