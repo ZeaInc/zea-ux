f8dbc6dcacc30d73a43521ceadffd1d9bf35e5e8
Philip Taylor <phil@hordesoftware.co>
2019-09-11 11:53:21 -0400

    fix: updates build and readme to new naming scheme.

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -5,14 +5,14 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'visualive-ux.js',
-    library: 'visualiveUx',
+    filename: 'zea-ux.js',
+    library: 'ZeaUx',
     libraryTarget: 'umd',
   },
   devtool: 'eval-source-map',
   externals: {
     '@jaames/iro': '@jaames/iro',
-    '@visualive/collab': '@visualive/collab',
-    '@visualive/engine': '@visualive/engine',
+    '@zeainc/zea-collab': '@zeainc/zea-collab',
+    '@zeainc/zea-engine': '@zeainc/zea-engine',
   },
 };
