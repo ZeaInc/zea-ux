751ff8a5f406c7df5a212cf74efa802691a5dce8
Froilan Moreno <fonofondo@gmail.com>
2020-01-28 11:30:45 -0500

    feat: add root scene tree view

---
+++ src/scene-tree-view/scene-tree-view.js
@@ -0,0 +1,31 @@
+class SceneTreeView extends HTMLElement {
+  treeItemView;
+  constructor() {
+    super();
+
+    var shadowRoot = this.attachShadow({ mode: 'open' });
+
+    // Add component CSS
+    const styleTag = document.createElement('style');
+    styleTag.appendChild(document.createTextNode(this.css));
+    shadowRoot.appendChild(styleTag);
+
+    // Create container tags
+    this.treeContainer = document.createElement('div');
+
+    //
+    shadowRoot.appendChild(this.treeContainer);
+    this.treeItemView = document.createElement('tree-item-view');
+  }
+
+  setTreeItem(treeItem, appData) {
+    this.treeItemView.setTreeItem(treeItem);
+    this.treeContainer.appendChild(this.treeItemView);
+  }
+}
+
+SceneTreeView.css = ``;
+
+customElements.define('scene-tree-view', SceneTreeView);
+
+export default SceneTreeView;
