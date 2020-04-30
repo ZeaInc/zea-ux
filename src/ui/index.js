ee7f6047c4d7948a1fe8bc9179a4b373f39c0f50
Philip Taylor <phil@zea.live>
2020-04-30 14:46:26 -0400

    Removed unused UI components.

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,17 +1,17 @@
 // ////////////////////////////////////
 // Parameter Widgets
-export * from './parameter-widgets/index'
+// export * from './parameter-widgets/index'
 
-export * from './top-menu-bar.js'
-export * from './side-panels.js'
-export * from './collab-panel.js'
+// export * from './top-menu-bar.js'
+// export * from './side-panels.js'
+// export * from './collab-panel.js'
 export * from './scene-tree-view.js'
-export * from './tree-item-inspector.js'
-export * from './parameter-container.js'
-export * from './inspector-container.js'
-export * from './action-tree-view.js'
-export * from './user-chip.js'
-export * from './publish-dialog.js'
+// export * from './tree-item-inspector.js'
+// export * from './parameter-container.js'
+// export * from './inspector-container.js'
+// export * from './action-tree-view.js'
+// export * from './user-chip.js'
+// export * from './publish-dialog.js'
 
 import uxFactory from './UxFactory.js'
 export { uxFactory }
