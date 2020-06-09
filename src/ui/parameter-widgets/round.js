20b96e72c3334fe4e094bc99f7a88db71cfe5b7b
Philip Taylor <phil@zea.live>
2020-06-09 17:13:11 -0400

    Removing classes no longer part of UX.  - All UI widgets are now handled by the zea-web-components library.

--- src/ui/parameter-widgets/round.js
+++
@@ -1,5 +0,0 @@
-// https://www.jacklmoore.com/notes/rounding-in-javascript/
-export default function round(value, decimals = 6) {
-  if (Math.abs(value) < Number('1e-6')) return 0
-  return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals)
-}
