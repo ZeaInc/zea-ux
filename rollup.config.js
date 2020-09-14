6b3b3e5317bee9758544e112e9624b89cccc71c2
Mauro Porras <mauroporrasc@gmail.com>
2020-09-14 08:55:38 -0500

    build(npm)!: Add UMD support (#78)
    BREAKING CHANGE: Raw imports are no longer supported.
    
    Close #78

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,35 @@ import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
 
-// Transforms imports for raw imports.
-// We use this for unit testing, so we can use the libraries
-// without needing a bundling tool like WebPack.
-// Raw imports require relative paths that can be resolved
-// from the location of the current module right to the esm module.
-// So we assume zea-cad is installed
-// node_modules\@zeainc\zea-cad\dist
-// And to get to zea-engine, we need to go up 2 folders and down into
-// the folder for zea-engine and find the esm.js file.
-// Normally WebPack uses the package.json file to resolve the file
-// at compile time, but we want to load modules dynamically
-const plugin = {
-  name: 'transform-imports',
-  renderChunk(code, chunk, options) {
-    return code.replace(
-      '@zeainc/zea-engine',
-      '../../zea-engine/dist/index.esm.js'
-    )
-  }
+const external = ['@zeainc/zea-engine']
+
+const plugins = []
+
+const isProduction = !process.env.ROLLUP_WATCH
+
+if (isProduction) {
+  plugins.push(terser())
 }
 
+const sourcemap = !isProduction
+
 export default [
+  // Browser-friendly UMD build.
+  {
+    input: 'src/index.js',
+    external,
+    output: {
+      name: 'zeaUx',
+      file: pkg.browser,
+      format: 'umd',
+      sourcemap,
+      globals: {
+        '@zeainc/zea-engine': 'zeaEngine',
+      },
+    },
+    plugins,
+  },
+
   // CommonJS (for Node) and ES module (for bundlers) build.
   // (We could have three entries in the configuration array
   // instead of two, but it's quicker to generate multiple
@@ -32,12 +39,11 @@ export default [
   // `file` and `format` for each target)
   {
     input: 'src/index.js',
-    external: [...Object.keys(pkg.dependencies)],
-    plugins: [terser()],
+    external,
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
-      { file: pkg.rawimport, format: 'es', plugins: [plugin] },
+      { file: pkg.main, format: 'cjs', sourcemap },
+      { file: pkg.module, format: 'es', sourcemap },
     ],
+    plugins,
   },
 ]
