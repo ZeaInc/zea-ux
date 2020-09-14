eb1ddacd8f959c4ed422182fc6a5d83f7ca10d4b
mauroporras <mauroporrasc@gmail.com>
2020-09-14 08:52:40 -0500

    WIP: umd-support

--- launch-e2e.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p testing-e2e/libs/zea-ux
-ln -fs $PWD/dist/ ./testing-e2e/libs/zea-ux/
-
-mkdir -p testing-e2e/libs/zea-engine
-ln -fs $PWD/node_modules/@zeainc/zea-engine/dist/ ./testing-e2e/libs/zea-engine/
-
-yarn run es-dev-server -r testing-e2e
