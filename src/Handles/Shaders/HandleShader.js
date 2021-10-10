1348fa4cc638c6e1a35394df82da8c1aa2ec6250
Philip Taylor <phil@zea.live>
2021-10-10 08:38:01 -0400

    fix: on Safari without rendering to float textures, we now support up to 7 registered passes.

--- src/Handles/Shaders/HandleShader.js
+++ src/Handles/Shaders/HandleShader.js
@@ -119,10 +119,8 @@ uniform int BaseColorTexType;
 
 #elif defined(DRAW_GEOMDATA)
 
-uniform int floatGeomBuffer;
-uniform int passId;
-
-<%include file="GLSLBits.glsl"/>
+uniform int isOrthographic;
+import 'surfaceGeomData.glsl'
 
 #elif defined(DRAW_HIGHLIGHT)
 
@@ -191,24 +189,7 @@ void main(void) {
   // GeomData
 #elif defined(DRAW_GEOMDATA)
 
-  float viewDist = length(v_viewPos);
-
-  if(floatGeomBuffer != 0) {
-    fragColor.r = float(passId); 
-    fragColor.g = float(v_drawItemId);
-    fragColor.b = 0.0;// TODO: store poly-id or something.
-    fragColor.a = viewDist;
-  } else {
-    ///////////////////////////////////
-    // UInt8 buffer
-    fragColor.r = mod(v_drawItemId, 256.) / 256.;
-    fragColor.g = (floor(v_drawItemId / 256.) + (float(passId) * 64.)) / 256.;
-
-    // encode the dist as a 16 bit float
-    vec2 float16bits = encode16BitFloatInto2xUInt8(viewDist);
-    fragColor.b = float16bits.x;
-    fragColor.a = float16bits.y;
-  }
+  fragColor = setFragColor_geomData(v_viewPos, floatGeomBuffer, passId, v_drawItemId, isOrthographic);
 
   //////////////////////////////////////////////
   // Highlight
