eac51911342277429fb531b3a412d79184700463
Seiji-zea Smith <seiji.smith@zea.live>
2022-01-14 11:38:56 -0800

    wip: members of appData are now optional

--- types/types.ts
+++ types/types.ts
@@ -1,16 +1,15 @@
-import { GLRenderer, Scene, Vec3 } from '@zeainc/zea-engine'
+import { GLRenderer, Scene, TreeItem, Vec3 } from '@zeainc/zea-engine'
 import { SelectionManager } from '../src/SelectionManager'
 
 export interface AppData {
-  renderer: GLRenderer
-  scene: Scene
-  selectionManager: SelectionManager
-  parentItem: TreeItem
-  session: any
+  renderer?: GLRenderer
+  scene?: Scene
+  selectionManager?: SelectionManager
+  parentItem?: TreeItem
+  session?: any
 }
 
 export interface PointerObject {
   grabPos: Vec3
   holdPos: Vec3
 }
-
